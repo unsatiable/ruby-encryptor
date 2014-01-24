9d594d2ece3061888b76d5c79663f872287e5c15
unsatiable <amadsen87@gmail.com>
2014-01-24 12:47:17 -0700

    finished encryptor, created decrypter

--- rubyencryptor.rb
+++ rubyencryptor.rb
@@ -1,27 +1,66 @@
 class Encryptor
 
-  def cipher
-    {'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
-     'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
-     'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
-     'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
-     'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
-     'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
-     'y' => 'l', 'z' => 'm', ' ' => '#'}
+  def cipher(rotation)
+    characters = (" ".."z").to_a
+    characters << "\n"
+    rotated_characters = characters.rotate(rotation)
+    Hash[characters.zip(rotated_characters)]
+   
   end
 
-  def encrypt(string)
+  def crack(string)
+    cipher(1).count.times do |i|
+      puts encrypt(string, i)
+    end
+    
+  end
+
+  def encrypt(string, rotation)
     letters = string.split("")
     encrypted_letters = letters.collect do |letter|
-      encrypted_letter = encrypt_letter(letter)
+      encrypt_letter(letter, rotation)
     end
 
-    encrypted_letters.join
+    encrypted_letters.join("")
   end
 
-  def encrypt_letter(letter)
-    lowercase_letter = letter.downcase
-    cipher[lowercase_letter]
+  def encrypt_file(filename, rotation)
+    # Creat the file handle to 
+    input = File.open(filename, "r")
+    # Read the text of the input file
+    text = input.read
+    # Encrypt the text
+    encrypted_text = encrypt(text,rotation)
+    # Create a name for the output file
+    new_name = filename + '.encrypted'
+    # Create an output file handle
+    new_name = File.open(new_name, "w")
+    # Write out the text
+    new_name.write(encrypted_text)
+    # Close the file
+    new_name.close
+  end
+    
+  def encrypt_letter(letter, rotation)
+    cipher_rotation = cipher(rotation)
+    cipher_rotation[letter]
+  end
+
+  def decrypt_file(filename, rotation)
+    # Creat the file handle to 
+    input = File.open(filename, "r")
+    # Read the text of the input file
+    text = input.read
+    # Encrypt the text
+    encrypted_text = encrypt(text,rotation)
+    # Create a name for the output file
+    new_name = filename + '.decrypted' 
+    # Create an output file handle
+    new_name = File.open(new_name, "w")
+    # Write out the text
+    new_name.write(encrypted_text)
+    # Close the file
+    new_name.close
   end
 
 end
